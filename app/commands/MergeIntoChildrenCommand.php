15604eb290db84d7e1b06b7081df341c0c3d5d52
Tim Strijdhorst <tim@decorrespondent.nl>
2020-09-25 14:02:53 +0200

    +option to recursively merge children

--- app/commands/MergeIntoChildrenCommand.php
+++ app/commands/MergeIntoChildrenCommand.php
@@ -7,6 +7,7 @@ use shakethatbranch\system\ChildRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class MergeIntoChildrenCommand extends Command {
@@ -30,21 +31,34 @@ class MergeIntoChildrenCommand extends Command {
 		parent::configure();
 		
 		$this->setDescription('Merges the current branch into it\'s children');
+		$this->addOption('recursive',['r'],InputOption::VALUE_NONE, 'Merge children of children');
 	}
 	
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		$currentBranchName = $this->gitRepository->getCurrentBranchName();
+		$this->mergeChildren($currentBranchName, $input->hasOption('recursive'));
 		
+		return Command::SUCCESS;
+	}
+	
+	/**
+	 * @param string $currentBranchName
+	 * @param bool   $recursive
+	 * @throws \Cz\Git\GitException
+	 */
+	protected function mergeChildren(string $currentBranchName, bool $recursive = false): void {
 		try {
-			foreach($this->childRepository->findChildren($currentBranchName) as $childBranchName) {
+			foreach ($this->childRepository->findChildren($currentBranchName) as $childBranchName) {
 				$this->gitRepository->checkout($childBranchName);
 				$this->gitRepository->merge($currentBranchName);
+				
+				if ($recursive && $this->childRepository->hasAnyChildren($childBranchName)) {
+					$this->mergeChildren($childBranchName, true);
+				}
 			}
 		}
 		finally {
 			$this->gitRepository->checkout($currentBranchName);
 		}
-		
-		return Command::SUCCESS;
 	}
 }
