660244c0badb3fce722dc9690ae751bd483da6b4
Tim Strijdhorst <tim@decorrespondent.nl>
2020-09-30 13:08:44 +0200

    %missing typehint

--- app/system/GitRepository.php
+++ app/system/GitRepository.php
@@ -3,7 +3,7 @@
 namespace shakethatbranch\system;
 
 class GitRepository extends \Cz\Git\GitRepository {
-	public function getPreviousBranchName() {
+	public function getPreviousBranchName() : string {
 		exec('git checkout -'.' 2>&1', $output, $ret);
 		preg_match('/Switched to branch \'(?<branchName>.+)\'/', $output[0], $matches);
 		exec('git checkout -'.' 2>&1', $output, $ret);
